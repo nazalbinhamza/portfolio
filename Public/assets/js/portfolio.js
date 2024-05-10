a8a1d228002169d2cf8e5905377dcb48d6a185e3
Nazal_bin_Hamza <151708027+nazalbinhamza@users.noreply.github.com>
2024-05-10 10:32:38 +0530

    add: new feature

--- Public/assets/js/portfolio.js
+++ Public/assets/js/portfolio.js
@@ -23,26 +23,3 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
-
-
-
-function sendMail(){
-    var params = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        message : document.getElementById("message").value,
-    };
-    const serviceID = "service_n8buzfp";
-    const templateID = "template_metmsu1";
-
-emailjs
-.send(serviceID,templateID,params)
-.then((res) =>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert ("your message sent successfully");
-    })
-    .catch((err) => console.log(err));
-}
